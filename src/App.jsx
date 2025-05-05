e4cfa48753a56d0d1d1f4008402dabdc65c5125e
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-05 19:21:01 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">Hello, World</h1>
+    </div>
+  )
+}
+
+export default App
