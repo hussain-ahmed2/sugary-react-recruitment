03b210df27963b593788abef657171a24ae750ad
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-05-06 18:33:27 +0600

    use the api for calling the post method for login

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,42 +1,23 @@
-import axios from "axios";
 import { useState } from "react";
-import { BASEAPIURL } from "../utils";
+import { api } from "../api/axios";
 import AuthContext from "./AuthContext";
 
 const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
-	const [isProcessing, setIsProcessing] = useState(false);
-	const [error, setError] = useState("");
 
 	const login = async (credentials) => {
-		try {
-			setIsProcessing(true);
+		const response = await api.post("/AdminAccount/Login", credentials);
 
-			const response = await axios.post(
-				`${BASEAPIURL}/AdminAccount/Login`,
-				credentials
-			);
+		const { Token, RefreshToken, User } = response.data;
 
-			const { Token, RefreshToken, User } = response.data;
+		localStorage.setItem("accessToken", Token);
+		localStorage.setItem("refreshToken", RefreshToken);
 
-			localStorage.setItem("accessToken", Token);
-			localStorage.setItem("refreshToken", RefreshToken);
-
-			setUser(User);
-		} catch (error) {
-			setError(error.response.data.Message);
-			return false;
-		} finally {
-			setIsProcessing(false);
-		}
-
-		return true;
+		setUser(User);
 	};
 
-    const resetError = () => setError('');
-
 	return (
-		<AuthContext.Provider value={{ user, login, isProcessing, error, resetError }}>
+		<AuthContext.Provider value={{ user, login }}>
 			{children}
 		</AuthContext.Provider>
 	);
